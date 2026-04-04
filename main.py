0ba5676265c5b50970347d52bc7b54a9d68cca95
Karthikraja-P <pkrkarthikraja@gmail.com>
2026-04-04 19:28:04 +0530

    skill set:sri

--- main.py
+++ main.py
@@ -168,5 +168,18 @@ def assessment_iq():
     return render_template('iq_assessment.html')
 
 
+@app.route('/assessment/skill')
+def assessment_skill():
+    return render_template('skill_selection.html')
+
+@app.route('/assessment/skill/ml')
+def assessment_skill_ml():
+    return render_template('skill_test.html', test_type='ml')
+
+@app.route('/assessment/skill/db')
+def assessment_skill_db():
+    return render_template('skill_test.html', test_type='db')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
